34a7ff30ef22f792b32afa0950f8be3081f41c0b
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-07-10 14:00:39 +0700

    change text and edit more contact.html

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 3,
-      slideShadows: true
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 8,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    // slideShadows: true
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1
     },
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+    768: {
+      slidesPerView: 1
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2
-      },
-      768: {
-        slidesPerView: 1
-      },
-      1024: {
-        slidesPerView: 2
-      },
-      1560: {
-        slidesPerView: 3
-      }
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
     }
-  });
+  }
+});
